ad8c3c700f69877f2a1775b0bb16f3ebec7a5cec
Raphael Ruíz dos Reis Castilho Costa <116287257+raphaelruizr@users.noreply.github.com>
2023-06-19 13:56:39 -0300

    resolvido 19-06-23
    uri1059 resolvido 19-06-23

---
+++ beecrowd-exercicios/src/uri1059/Uri1059.java
@@ -0,0 +1,14 @@
+package uri1059;
+
+public class Uri1059 {
+
+	public static void main(String[] args) {
+
+		for (int x = 1, y = 100; x <= y; x++) {
+			if (x % 2 == 0)
+				System.out.println(x);
+		}
+
+	}
+
+}
